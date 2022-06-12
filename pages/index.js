ddc94a972d41b09f281455dc3cc6f1e55fbb9bd2
Zen0sama <vt2104@gmail.com>
2022-06-12 18:03:39 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="">
+            <main className="bg-slate-700 min-h-screen flex justify-center items-center">
+                <h1 className="text-white font-bold text-4xl">WIBU SAMA</h1>
+            </main>
+        </div>
+    )
+}

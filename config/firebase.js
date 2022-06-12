271f3b69dee6e2a80e6e7d5a2ad1745e55e4540d
Zen0sama <vt2104@gmail.com>
2022-06-12 18:50:23 +0700

    install firebase

---
+++ config/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app'
+const firebaseConfig = {
+    apiKey: 'AIzaSyBE-hgWXhjDylu5Tuo-UiuOkhO_3k4Njgc',
+    authDomain: 'fir-vps.firebaseapp.com',
+    projectId: 'fir-vps',
+    storageBucket: 'fir-vps.appspot.com',
+    messagingSenderId: '84746679205',
+    appId: '1:84746679205:web:9af82399d165f1561bee4c',
+}
+const app = initializeApp(firebaseConfig)
